96de77e0e487399f7973d9f4dd3300f13497383b
unicornDi <kochubeyalex71@gmail.com>
2019-02-26 22:12:53 +0100

    final commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var pug = require('gulp-pug');
 var browserSync = require('browser-sync').create();
+
+gulp.task('pug', function() {
+    return gulp.src('source/pug/pages/index-page/*.pug')
+    .pipe(pug({
+        pretty:true
+    }))
+    .pipe(gulp.dest('public'))
+    .pipe(browserSync.reload({
+        stream: true
+    }))
+});
 
 gulp.task('sass', function() {
     return gulp.src('source/sass/style.scss')
@@ -10,3 +22,26 @@ gulp.task('sass', function() {
         stream: true
     }))
 });
+
+gulp.task('server', function () {
+    browserSync.init({
+      server: 'public/',
+      notify: false,
+      open: true,
+      cors: true,
+      ui: false
+    });
+
+    gulp.watch('source/pug/**/*.pug' , gulp.series('pug'));
+    gulp.watch('source/sass/**/*.{scss,sass}', gulp.series('sass'));
+});
+
+gulp.task("copy", function () {
+    return gulp.src([
+    "source/fonts/**/*.{eot,ttf,woff,woff2}" 
+     ], {
+       base: "source"
+     })
+     .pipe(gulp.dest("public"));
+   });
+gulp.task('start', gulp.series('copy', 'pug','sass','server'));
